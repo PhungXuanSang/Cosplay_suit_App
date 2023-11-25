39330c94f2998c59042a630f2bf27c747c1d2e0f
sangpxph26484 <sangpxph26484@fpt.edu.vn>
2023-11-25 13:37:23 +0700

    commit so luong

--- app/src/main/java/com/example/cosplay_suit_app/Adapter/QlspAdapter.java
+++ app/src/main/java/com/example/cosplay_suit_app/Adapter/QlspAdapter.java
@@ -46,7 +46,7 @@ public class QlspAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         }else {
             viewHolder.tvName.setText(sanPham.getNameproduct() +"--"+ sanPham.getSize());
         }
-//        viewHolder.tvAmount.setText(sanPham.getAmount());
+        viewHolder.tvAmount.setText(String.valueOf(sanPham.getAmount()));
         viewHolder.tvPrice.setText(sanPham.getPrice()+"");
 
         if (sanPham.getListImage() != null && !sanPham.getListImage().isEmpty()) {
